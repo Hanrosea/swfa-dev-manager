68facb3622838651604dc200dd5834f01a47c5e2
HanRoseA <112860641+Hanrosea@users.noreply.github.com>
2026-08-13 07:10:47 +0000

    S0010

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["172.1.5.16"],
+  allowedDevOrigins: [
+    "127.0.0.1",
+    "localhost",
+    "172.1.5.16",
+    "*.app.github.dev",
+  ],
 };
 
 export default nextConfig;
